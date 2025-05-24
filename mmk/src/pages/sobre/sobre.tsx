13b0ead042923feafebd3dc7eddba679af214a54
Maycon Douglas <maycon2005cs@gmail.com>
2025-05-24 15:40:50 -0300

    Adição de Imagem
    adiçao de imagem na pagina sobre

--- mmk/src/pages/sobre/sobre.tsx
+++ mmk/src/pages/sobre/sobre.tsx
@@ -1,8 +1,12 @@
 import "./sobre.css";
+import Foto from "../../imagem/Captura de Tela (259).png"
 
 export function Sobre(){
     return(
         <div>
+            <div className="Foto">
+            <img src={Foto} alt="" />
+            </div>
 
         </div>
     )
