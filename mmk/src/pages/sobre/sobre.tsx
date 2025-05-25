bbdd8f01f6243c7d274421cecf52bb7c4eb5afb8
Maycon Douglas <maycon2005cs@gmail.com>
2025-05-24 23:26:06 -0300

    modificação da pagina sobre
    alteração da imagem inicial

--- mmk/src/pages/sobre/sobre.tsx
+++ mmk/src/pages/sobre/sobre.tsx
@@ -1,13 +1,19 @@
 import "./sobre.css";
-import Foto from "../../imagem/Captura de Tela (259).png"
+import img from "../../imagem/Captura de Tela (259).png"
 
 export function Sobre(){
     return(
         <div>
+            <div className="*"></div>
+            <div className="page-wrapper"></div>
             <div className="Foto">
-            <img src={Foto} alt="" />
+            <div className="overlay" />
+            <div className="content-center"></div>
+            <h1 className="titulo">NOSSA COMPANHIA</h1>
+            <img src={img} alt="" />
             </div>
-
+   
         </div>
+
     )
 }
